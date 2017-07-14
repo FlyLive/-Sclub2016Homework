b3ab123d681bb1f83b12908b6cb824044a3397dd
FlyLive <1585213801@qq.com>
2017-07-14 12:02:50 +0800

    Calculator单元测试

---
+++ Sclub2016Homework/ChangeProblem/ChangeProblemLib.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChangeProblem
+{
+    public class ChangeProblemLib
+    {
+        public int Multi(int x, int y)
+        {
+            return x * y;
+        }
+    }
+}
---
+++ Sclub2016Homework/ChangeProblem/ChangeProblemTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace ChangeProblem
+{
+    public class ChangeProblemTest
+    {
+        private readonly ChangeProblemLib calculator;
+
+        public ChangeProblemTest()
+        {
+            calculator = new ChangeProblemLib();
+        }
+
+        [Fact]
+        public void OneMutiOneIsOne()
+        {
+            var result = calculator.Multi(1, 1);
+            Assert.Equal(1, result);
+        }
+
+        [Theory]
+        [InlineData(-1)]
+        [InlineData(0)]
+        [InlineData(1)]
+        public void ReturnValue(int value)
+        {
+            var result = calculator.Multi(1, value);
+
+            Assert.Equal(result, value);
+        }
+    }
+}
