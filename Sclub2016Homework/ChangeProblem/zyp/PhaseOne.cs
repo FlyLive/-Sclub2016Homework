b9ce7848c532fb247453e5af69e6ab4068beb362
FlyLive <1585213801@qq.com>
2017-07-17 11:55:37 +0800

    找零问题

---
+++ Sclub2016Homework/ChangeProblem/zyp/PhaseOne.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace ChangeProblem
+{
+    class PhaseOne
+    {
+        //计算应找零钱
+        public decimal Balance(decimal payMoney, decimal price)
+        {
+            return payMoney - price;
+        }
+        //计算最优应找零钱的张数
+        public int[] OptimalNumOfChange(decimal change, decimal[] denomination)
+        {
+            int[] numOfChange = new int[denomination.Length];
+            for (int i = 0; i < denomination.Length; i++)
+            {
+                numOfChange[i] = (int)(change / denomination[i]);
+                change = (decimal)(change % denomination[i]);
+            }
+            return numOfChange;
+        }
+    }
+}
---
+++ Sclub2016Homework/ChangeProblem/zyp/PhaseTwo.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace ChangeProblem
+{
+    class PhaseTwo
+    {
+        //计算应找零钱
+        public decimal Balance(decimal pay, decimal money)
+        {
+            return pay - money;
+        }
+        //根据钱柜实际应找零钱的张数
+        public int[] ActualNumOfChange(decimal balance, decimal[] denomination,int[] numOfBox)
+        {
+            int[] numOfChange = new int[denomination.Length];
+            for (int i = 0; i < denomination.Length; i++)
+            {
+                if (numOfBox[i] != 0)
+                {
+                    numOfChange[i] = (int)(balance / denomination[i]);
+                    balance = (decimal)(balance % denomination[i]);
+                }
+            }
+            return numOfChange;
+        }
+        //找零钱后，钱柜的变化
+        public int[] AddMoney(int[] numOfChange, int[] numOfPay, int[] numOfBox)
+        {
+            for (int i = 0; i < numOfChange.Length; i++)
+            {
+                numOfBox[i] += numOfPay[i] - numOfChange[i];
+            }
+            return numOfBox;
+        }
+    }
+}
